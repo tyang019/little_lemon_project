09fa71005184502c5c9bb6af89510f0a0ad89378
tyang019 <tyang019@fiu.edu>
2025-11-08 04:59:16 -0500

    Initialized new changes

--- script.js
+++ script.js
@@ -20,24 +20,22 @@ searchButton.addEventListener('click', ()=> {
 
 //Create Menu
  const menuItems = [
-  { name: "Banana Ice Cream", price: 23.99 },
-  { name: "Strawberry Gelato", price: 19.99 },
-  { name: "Greek Yogurt Delight", price: 17.49 },
+  {name: "Banana Ice Cream", calories:"180 kcal", price: 23.99, image:"logo/banana.png", paragraph:"A smooth and creamy frozen dessert made from ripe bananas, milk, and a touch of sugar for natural sweetness."},
+  {name: "Strawberry Gelato", calories:"160 kcal", price: 19.99, image:"logo/strawberry.png", paragraph:"Strawberry Gelato is a creamy Italian-style frozen dessert made with ripe, sun-sweetened strawberries, fresh whole milk, and cream."},
+  {name: "Greek Yogurt Delight", calories:"150 kcal", price: 17.49, image:"logo/greek.png", paragraph:"A rich and tangy dessert made with thick Greek yogurt, honey, and fresh fruits like berries or nuts for added flavor and texture."},
 ];
 
 const section = document.getElementById('section_menu');
 menuItems.forEach(item => {
   const article = document.createElement('article');
-  article.innerHTML = `
-  <div>
-    <h3>${item.name}</h3>
+  article.innerHTML = 
+  `<div class="bc_menu">
+  <h3>${item.name} - ${item.calories}</h3>
+    <img src="${item.image}" class="menu-image">
     <p>$${item.price}</p>
-    <button>Add to cart</button>
+    <p class="menu_description">${item.paragraph}</p>
+    <button class="menuBtn">Add to cart</button>
     </div>
-    <div>
-    <h3>${item.name}</h3>
-    <p>$${item.price}</p>
-    <button>Add to cart</button>
-    </div>`;
+    `;
     section.appendChild(article);
 })
