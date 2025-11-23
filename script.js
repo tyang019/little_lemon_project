8cf762afa0f611514662ac0b3ffb857673444349
tyang019 <tyang019@fiu.edu>
2025-11-23 01:37:51 -0500

    Initialized new files and code

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ const input = document.getElementById('input');
 const searchButton = document.querySelector('.search-button');
 const overlay = document.getElementById('overlay');
 const submitBtn = document.getElementById('submitBtn');
-const cartBtn = document.getElementById('cartBtn');
-const addToCartBtn = document.querySelectorAll('.menuBtn');
+const cartLogo= document.getElementById('cartLogo');
 
 //2. Add Event Listener
 submitBtn.addEventListener('click', function(){
@@ -19,14 +18,8 @@ searchButton.addEventListener('click', ()=> {
   }
 });
 
-document.querySelectorAll('.menuBtn').forEach(btn => {
-  btn.addEventListener('click', () => {
-    alert("Item added to cart!");
-  });
-});
 
-
-//Create Menu
+//Create Menu using arrays
 const menuItems2 = [
   {
     category: "Starters", img: "logo/starters2.jpg", name:"Papaya Salad", price: 8.95, name1: "Baked Assorted Vegetables", price1: 11.67, name2: "Tom Yum Goong", price2: 12.99
@@ -45,16 +38,15 @@ const menuItems2 = [
   }
 ]
  const menuItems = [
-  {name: "Lava Flow", calories: "350 kcal", price: 16.95, image: "logo/lava_flow.jpg", paragraph: "A tropical blended cocktail made with rum, coconut cream, pineapple juice, and strawberries, creating a creamy and fruity drink with a vibrant presentation.",
-  },{name: "Strawberry Bourbon Cocktail", calories: "300 kcal", price: 13.75, image: "logo/beverages (2).jpg", paragraph: "A refreshing cocktail made with bourbon, fresh strawberries, lemon juice, and simple syrup, served over ice and garnished with a strawberry slice.",},{name: "Navam", calories: "200 kcal", price: 4.25, image: "logo/navam.jpg", paragraph: "A traditional Thai herbal drink made from a blend of herbs and spices, known for its health benefits and unique flavor profile.",},{name: "Cranberry Spring Salad", calories: "150 kcal", price: 13.87, image: "logo/cranberry-salad.jpg", paragraph: "A fresh salad made with mixed greens, dried cranberries, candied pecans, feta cheese, and a tangy vinaigrette dressing.",
-  },{name: "Macaroni Cheese", calories: "400 kcal", price: 15.65, image: "logo/macaroni-cheese.jpg", paragraph: "A creamy and cheesy pasta dish made with elbow macaroni, cheddar cheese sauce, and topped with breadcrumbs for a crispy finish.",
-  },{name: "Steamed Fish", calories: "300 kcal", price: 25.85, image: "logo/steamed-fish.jpg", paragraph: "A whole fish steamed with ginger, garlic, soy sauce, and scallions, resulting in tender, flavorful meat infused with aromatic seasonings.",
-  },{name: "Tom Yum Goong", calories: "250 kcal", price: 12.99, image: "logo/tomyum.jpg", paragraph: "A spicy and sour Thai soup made with shrimp, lemongrass, kaffir lime leaves, galangal, chili, and lime juice, creating a bold and aromatic flavor profile."
-  },{name: "Baked Assorted Vegetables", price: 11.67, image: "logo/baked-vegetables.jpg", paragraph: "A medley of roasted vegetables seasoned with herbs and spices, creating a flavorful and healthy side dish."},{name:"Papaya Salad", calories: "190 kcal", price: 8.95, image: "logo/starters2.jpg", paragraph: "A refreshing Thai salad made from shredded green papaya, tomatoes, carrots, chili, lime juice, fish sauce, and crushed peanuts for a spicy-sweet balance."
-  },{name: "Stuffed Turkey", calories:"190 kcal", price: 35.95, image:"logo/turkey.png", paragraph:"A roasted whole turkey seasoned with herbs, butter, and spices, with Asian-style stuffing and buttery gravy"},{name: "Mashed Potato Gravy", calories:"230 kcal", price: 25.95, image:"logo/mashed-potatoes.jpg", paragraph:"Boiled potatoes blended and mashed with butter, milk or cream, and salted until smooth and fluffy, topped with vegetable herbs and gravy"},
-  {name: "Grilled Steak", calories:"550 kcal", price: 30.95, image:"logo/steak.jpg", paragraph:"A tender cut of beef—seasoned with salt, pepper, and herbs,  for a smoky, caramelized flavor and juicy interior."},{name: "Banana Ice Cream", calories:"180 kcal", price: 23.99, image:"logo/banana.png", paragraph:"A smooth and creamy frozen dessert made from ripe bananas, milk, and a touch of sugar for natural sweetness."},{name: "Strawberry Gelato", calories:"160 kcal", price: 19.99, image:"logo/strawberry.png", paragraph:"Strawberry Gelato is a creamy Italian-style frozen dessert made with ripe, sun-sweetened strawberries, fresh whole milk, and cream."},{name: "Greek Yogurt Delight", calories:"150 kcal", price: 17.49, image:"logo/greek.png", paragraph:"A rich and tangy dessert made with thick Greek yogurt, honey, and fresh fruits like berries or nuts for added flavor and texture."}
+  {name: "Lava Flow", calories: 350, price: 16.95, image: "logo/lava_flow.jpg", paragraph: "A tropical blended cocktail made with rum, coconut cream, pineapple juice, and strawberries, creating a creamy and fruity drink with a vibrant presentation.",
+  },{name: "Strawberry Bourbon Cocktail", calories: 300, price: 13.75, image: "logo/beverages (2).jpg", paragraph: "A refreshing cocktail made with bourbon, fresh strawberries, lemon juice, and simple syrup, served over ice and garnished with a strawberry slice.",},{name: "Navam", calories: 200, price: 4.25, image: "logo/navam.jpg", paragraph: "A traditional Thai sweet drink made from a blend of coconut and jelly, known for its health benefits and unique flavor profile of colorful jellies and assorted flavors.",},{name: "Cranberry Spring Salad", calories: 150, price: 13.87, image: "logo/cranberry-salad.jpg", paragraph: "A fresh salad made with mixed greens, dried cranberries, candied pecans, feta cheese, and a tangy vinaigrette dressing.",
+  },{name: "Macaroni Cheese", calories: 400, price: 15.65, image: "logo/macaroni-cheese.jpg", paragraph: "A creamy and cheesy pasta dish made with elbow macaroni, cheddar cheese sauce, and topped with breadcrumbs for a crispy finish.",
+  },{name: "Steamed Fish", calories: 300, price: 25.85, image: "logo/steamed-fish.jpg", paragraph: "A whole fish steamed with ginger, garlic, soy sauce, and scallions, resulting in tender, flavorful meat infused with aromatic seasonings.",
+  },{name: "Tom Yum Goong", calories: 250, price: 12.99, image: "logo/tomyum.jpg", paragraph: "A spicy and sour Thai soup made with shrimp, lemongrass, kaffir lime leaves, galangal, chili, and lime juice, creating a bold and aromatic flavor profile."
+  },{name: "Baked Assorted Vegetables", calories: 135, price: 11.67, image: "logo/baked-vegetables.jpg", paragraph: "A medley of roasted vegetables seasoned with herbs and spices, creating a flavorful and healthy side dish."},{name:"Papaya Salad", calories: 190, price: 8.95, image: "logo/starters2.jpg", paragraph: "A refreshing Thai salad made from shredded green papaya, tomatoes, carrots, chili, lime juice, fish sauce, and crushed peanuts for a spicy-sweet balance."
+  },{name: "Stuffed Turkey", calories:330, price: 35.95, image:"logo/turkey.png", paragraph:"A roasted whole turkey seasoned with herbs, butter, and spices, with Asian-style stuffing and buttery gravy"},{name: "Mashed Potato Gravy", calories:250, price: 25.95, image:"logo/mashed-potatoes.jpg", paragraph:"Boiled potatoes blended and mashed with butter, milk or cream, and salted until smooth and fluffy, topped with vegetable herbs and gravy"},
+  {name: "Grilled Steak", calories:550, price: 30.95, image:"logo/steak.jpg", paragraph:"A tender cut of beef—seasoned with salt, pepper, and herbs,  for a smoky, caramelized flavor and juicy interior."},{name: "Banana Ice Cream", calories:180, price: 23.99, image:"logo/banana.png", paragraph:"A smooth and creamy frozen dessert made from ripe bananas, milk, and a touch of sugar for natural sweetness."},{name: "Strawberry Gelato", calories:160, price: 19.99, image:"logo/strawberry.png", paragraph:"Strawberry Gelato is a creamy Italian-style frozen dessert made with ripe, sun-sweetened strawberries, fresh whole milk, and cream."},{name: "Greek Yogurt Delight", calories:150, price: 17.49, image:"logo/greek.png", paragraph:"A rich and tangy dessert made with thick Greek yogurt, honey, and fresh fruits like berries or nuts for added flavor and texture."}
 ];
-
 
 
 
